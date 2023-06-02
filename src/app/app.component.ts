a127a545ab97adaeecdee1cbcbe4fa1d6f9bcef8
Pedrocardo005 <pedrocardo007@hotmail.com>
2023-06-01 22:29:11 -0300

    Add estrutura inicial do getValue

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,7 +15,31 @@ export class AppComponent {
 
   getValue(evento: any) {
     const valor = String(evento.target.value);
-    var nada = '';
+    let len = this.digitadas.length;
+
+    // Impede que operações venham antes que números
+    if (this.operacoes.indexOf(valor) >= 0 && !this.digitadas) {
+      return;
+    }
+
+    // Impede que o usuário digite operação seguida de operação
+    if (len && 
+        this.operacoes.indexOf(this.digitadas[len-1]) >= 0 &&
+        this.operacoes.indexOf(valor) >= 0) {
+      return;
+    }
+
+    if (valor === "=") {
+      // Percorrer digitadas até parar em uma operação
+      // Converter aqueles caracteres para número de fato
+      // Realizar a operação daqueles números
+      // Penso em realizar com uma função recursiva as operações 
+    }
+
+    if (valor === "=" || valor === ".") {
+      return;
+    }
+    
     this.digitadas += valor;
   }
 }
